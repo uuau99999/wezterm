f3555997a4b97c57c7c69e8c6dbedc8cf2df537b
hoyup <hoyupye@tencent.com>
2024-04-25 18:50:15 +0800

    chore: update wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -7,11 +7,11 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "catppuccin-mocha"
+config.color_scheme = "Kanagawa (Gogh)"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 16
-config.window_background_opacity = 0.7
-config.macos_window_background_blur = 15
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
 
 -- and finally, return the configuration to wezterm
 return config
