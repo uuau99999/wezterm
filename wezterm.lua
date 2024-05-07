f5b1817e84c9a39632457f0aeb151b208cf2e445
hoyup <hoyupye@tencent.com>
2024-05-07 09:26:16 +0800

    chore: update color scheme as nightfly

--- wezterm.lua
+++ wezterm.lua
@@ -7,11 +7,14 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Kanagawa (Gogh)"
+config.color_scheme = "Nightfly (Gogh)"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 16
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
+config.colors = {
+	cursor_bg = "#21c7a8",
+}
 
 -- and finally, return the configuration to wezterm
 return config
